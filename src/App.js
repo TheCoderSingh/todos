eaaebd28eecf89b127842deb70d55ae74a8e460a
Jaskaran Singh <jaskaran1523@gmail.com>
2021-07-28 23:37:05 -0700

    Add colors, create To Do List header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.scss";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<section id="App-title">
+				<h1>To Do List</h1>
+			</section>
+		</div>
+	);
 }
 
 export default App;
